99bcde48fb5230481071d8cc1ac8b48aa1a7b8ee
Yunji Kim <normaltic@gmail.com>
2022-05-22 21:20:31 +0900

    Add export loadPostcode function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import DaumPostcodeEmbed, { DaumPostcodeEmbedProps, DaumPostcodeProps } from './DaumPostcodeEmbed';
 import useDaumPostcodePopup, { PopupOptions } from './useDaumPostcodePopup';
-import { Address, Search, State } from './loadPostcode';
+import loadPostcode, { Address, Search, State } from './loadPostcode';
 
 export type { DaumPostcodeEmbedProps, DaumPostcodeProps, PopupOptions, Address, Search, State };
-export { DaumPostcodeEmbed, useDaumPostcodePopup };
+export { loadPostcode, DaumPostcodeEmbed, useDaumPostcodePopup };
 export default DaumPostcodeEmbed;
