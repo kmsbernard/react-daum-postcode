f8528cb11ba3202b9c17393bba9fb13c8a5c5abb
Yunji Kim <normaltic@gmail.com>
2024-12-12 17:10:30 +0900

    Fix to separate types from import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import DaumPostcodeEmbed, { DaumPostcodeEmbedProps, DaumPostcodeProps } from './DaumPostcodeEmbed';
-import useDaumPostcodePopup, { DaumPostcodePopupParams } from './useDaumPostcodePopup';
-import loadPostcode, { Address, Search, State } from './loadPostcode';
+import DaumPostcodeEmbed from './DaumPostcodeEmbed';
+import useDaumPostcodePopup from './useDaumPostcodePopup';
+import loadPostcode from './loadPostcode';
 
-export type { DaumPostcodeEmbedProps, DaumPostcodeProps, DaumPostcodePopupParams, Address, Search, State };
 export { loadPostcode, DaumPostcodeEmbed, useDaumPostcodePopup };
 export default DaumPostcodeEmbed;
+
+import type { DaumPostcodeEmbedProps, DaumPostcodeProps } from './DaumPostcodeEmbed';
+import type { DaumPostcodePopupParams } from './useDaumPostcodePopup';
+import type { Address, Search, State } from './loadPostcode';
+
+export type { DaumPostcodeEmbedProps, DaumPostcodeProps, DaumPostcodePopupParams, Address, Search, State };
